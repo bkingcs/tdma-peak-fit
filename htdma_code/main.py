233de65a4afe5f5cbf660e6cae6b4f10c19fa9a1
Brian King <brk009@bucknell.edu>
2023-09-24 20:11:34 -0400

    20230925.01 Fixed the resolution error on High DPI Windows machines.

--- htdma_code/main.py
+++ htdma_code/main.py
@@ -3,6 +3,7 @@ import sys
 from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
 
 import sys
+from PySide2 import QtCore
 from PySide2.QtWidgets import QApplication
 
 from htdma_code.controller.controller import Controller
@@ -10,9 +11,16 @@ from htdma_code.model.model import Model
 from htdma_code.view.main_window import MainWindow
 
 # Main version number for the software
-SW_VERSION="20221021.01"
+SW_VERSION="20230925.01"
 
 if __name__ == '__main__':
+
+    if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+        QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+    if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+        QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
     app = QApplication(sys.argv)
 
     # Instantiate a model
